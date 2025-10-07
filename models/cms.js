bdda5ed2f0fbab4b5cae7c90c92c26d3bb4fae7d
DevRamPandey <developerRamPandey@gmail.com>
2025-10-07 19:59:47 +0530

    Update cms.js

--- models/cms.js
+++ models/cms.js
@@ -1,16 +1,16 @@
 const mongoose = require('mongoose');
 
 const faqSchema = new mongoose.Schema({
-  question: { type: String, required: true },
-  answer: { type: String, required: true },
+  question: { type: String, required: false },
+  answer: { type: String, required: false },
 });
 
 const cmsSchema = new mongoose.Schema({
-  brandStory: { type: String, required: true },
-  termsConditions: { type: String, required: true },
-  privacyPolicy: { type: String, required: true },
+  brandStory: { type: String, required: false },
+  termsConditions: { type: String, required: false },
+  privacyPolicy: { type: String, required: false },
   faqs: { type: [faqSchema], default: [] },
-  refundPolicy: { type: String, required: true },
+  refundPolicy: { type: String, required: false },
 }, { timestamps: true });
 
 module.exports = mongoose.model('CMS', cmsSchema);
