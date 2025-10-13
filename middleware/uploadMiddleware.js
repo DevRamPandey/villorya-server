eec94eb20bf2f2095edd2ccf579ef27d32aa0998
RamPandey <developerRamPandey@gmail.com>
2025-10-13 06:07:04 +0530

    update upload

--- middleware/uploadMiddleware.js
+++ middleware/uploadMiddleware.js
@@ -3,7 +3,6 @@ const multer = require("multer");
 const storage = multer.memoryStorage(); // store file in memory
 const upload = multer({
   storage,
-  dest: "/tmp/",
   fileFilter: (req, file, cb) => {
     if (file.mimetype === "application/pdf") cb(null, true);
     else cb(new Error("Only PDF files allowed"));
